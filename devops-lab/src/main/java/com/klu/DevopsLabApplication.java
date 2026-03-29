940785c88cfacf06da10f0e6f0044577f4ea00d2
Nitisha33340 <2400033340@kluniversity.in>
2026-03-29 10:05:27 +0530

    Added DevOps lab project

---
+++ devops-lab/src/main/java/com/klu/DevopsLabApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsLabApplication.class, args);
+	}
+
+}
